18de15ebaf2d4bd5bc13c752d37d761b12fe7d5a
이재문 <gmrfyd912@gmail.com>
2026-05-25 08:11:46 +0900

    chore: Firebase config 초기 세팅 및 연결

---
+++ src/services/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAWf_Zpn-J_i0kLIKJg0rwWpEdtrA_rhaM",
+  authDomain: "ai-gency-82d6d.firebaseapp.com",
+  projectId: "ai-gency-82d6d",
+  storageBucket: "ai-gency-82d6d.firebasestorage.app",
+  messagingSenderId: "276098673565",
+  appId: "1:276098673565:web:19cf6c4541dd37cc5f1d74"
+};
+
+// Firebase 초기화
+const app = initializeApp(firebaseConfig);
+
+export default app;
